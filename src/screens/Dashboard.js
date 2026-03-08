cc2afd52ee081ff7ab9345f01d5e0c43d0e5d0a1
Harsh Mayekar <harshmayekar0801@gmail.com>
2026-03-08 22:20:20 +0530

    Add src structure and navigation dependencies

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard - Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
